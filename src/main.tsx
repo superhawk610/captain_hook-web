36c0d43b2be53e56a1220679c7459ba35e364b1f
Aaron Ross <superhawk610@gmail.com>
2019-04-22 17:42:18 -0400

    add SCSS and normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import 'normalize.css';
+import './styles.scss';
+
 import App from './components/App';
 
 const root = document.getElementById('root');
